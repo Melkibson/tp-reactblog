5763adab458ac2d3b3f1b81bd530ec3706062121
Melkibson <melkiyamna@protonmail.com>
2022-06-01 16:17:19 +0200

    utiliser methode getPosts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,27 +1,16 @@
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {getPosts} from "../../utils/requests";
 
 const Post = () => {
     const [post, setPost] = useState({});
     const postId = useParams().id;
     const path = `http://localhost:3001/posts/${postId}`;
-    const getPost = () => {
-        fetch(path, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
-            .then(res => res.json())
-            .then(data => setPost(data))
-            .catch(err => console.log(err));
-    }
     useEffect(() => {
-        getPost();
+        getPosts(setPost, path);
     }, [])
   return (
     <div className="container">
-
       <h1>{post.title}</h1>
         <div className="col-12">
             <div className="container">
@@ -29,7 +18,6 @@ const Post = () => {
             </div>
             <p className="text-muted">{post.author}</p>
             <small className="text-muted">Genre: {post.genre}</small>
-
         </div>
         <div className="col pt-5 pb-3">
             <Link to="/posts" className="btn btn-outline-primary">Tous les articles</Link>
